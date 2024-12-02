b101830a1a32edf50f18e1e58575718acc345d4c
Cédric <49195326+ce-dr@users.noreply.github.com>
2024-12-02 11:09:33 +0100

    refactor

---
+++ csharp/aoc2024/aoc2024/Puzzles/Solution.cs
@@ -0,0 +1,20 @@
+﻿namespace aoc2024.Puzzles;
+
+public abstract class Solution
+{
+    private readonly string _day;
+    protected Solution(string day)
+    {
+        _day = day;
+    }
+
+    public abstract string SolvePartOne();
+
+    public abstract string SolvePartTwo();
+
+    public void Solve()
+    {
+
+        Console.WriteLine($"Solution {_day}.1: {SolvePartOne()}; Solution {_day}.2: {SolvePartTwo()}");
+    }
+}
