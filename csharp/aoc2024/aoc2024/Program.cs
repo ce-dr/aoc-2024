b98f3b9ca954d6ecee0eb8b6e765e055c4250db2
Cédric <49195326+ce-dr@users.noreply.github.com>
2024-12-03 21:25:49 +0100

    solve day 3

--- csharp/aoc2024/aoc2024/Program.cs
+++ csharp/aoc2024/aoc2024/Program.cs
@@ -1,8 +1,9 @@
 ﻿using aoc2024.Puzzles;
 using aoc2024.Puzzles.Day1;
 using aoc2024.Puzzles.Day2;
+using aoc2024.Puzzles.Day3;
 
-List<Solution> solutions = [new Day1(), new Day2()];
+List<Solution> solutions = [new Day1(), new Day2(), new Day3()];
 
 foreach (Solution solution in solutions)
 {
